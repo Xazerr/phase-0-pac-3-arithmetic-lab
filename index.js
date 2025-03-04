a2f6d1febff5df7ccece133e95754a68f852e199
jeffreyxazer <jeffreyxazer@gmail.com>
2025-03-04 13:10:08 +0300

    assignment complete

---
+++ index.js
@@ -0,0 +1,53 @@
+function add() {
+  // we'll fill this in shortly
+}
+
+function subtract() {
+  // we'll fill this in shortly
+}
+
+function multiply() {
+  // we'll fill this in shortly
+}
+
+function divide() {
+  // we'll fill this in shortly
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function subtract(b, a) {
+  return b - a;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function increment(n) {
+  return n + 1;
+}
+
+
+function decrement(n) {
+  return n - 1;
+}
+
+function makeInt(n) {
+  const parsed = parseInt(n, 10);
+  return isNaN(parsed) ? NaN : parsed;
+}
+
+function preserveDecimal(n) {
+  const parsed = parseFloat(n);
+  return isNaN(parsed) ? NaN : parsed;
+}
+
+
+
